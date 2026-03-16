4b36d487995da438b9fb4f6e2661e618d702dabf
Fabio Bonelli <fb@fabiobonelli.it>
2026-03-16 21:20:02 +0100

    feat: expose --no-network and --no-external-checks flags
    Fix #33.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,19 @@
 
 set -o pipefail
 
+EXTRA_ARGS=""
+
+if [ "$3" == "true" ]; then
+    EXTRA_ARGS="$EXTRA_ARGS --no-network"
+fi
+
+if [ "$4" == "true" ]; then
+    EXTRA_ARGS="$EXTRA_ARGS --no-external-checks"
+fi
+
 # comment-on-pr parameter from the action
 if [ "$2" == "true" ]; then
-    publiccode-parser "$1" | reviewdog -efm="%f:%l:%c: %t%*[^:]: %m" -reporter=github-pr-review -tee
+    publiccode-parser $EXTRA_ARGS "$1" | reviewdog -efm="%f:%l:%c: %t%*[^:]: %m" -reporter=github-pr-review -tee
 else
-    publiccode-parser "$1"
- fi
+    publiccode-parser $EXTRA_ARGS "$1"
+fi
