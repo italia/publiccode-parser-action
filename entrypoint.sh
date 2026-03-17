6d242101f0b6879c343953e03513912ff240e666
Fabio Bonelli <fb@fabiobonelli.it>
2026-03-17 08:23:53 +0100

    refactor: use named variables for entrypoint args

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,23 @@
 
 set -o pipefail
 
+PUBLICCODE_YML_PATH="$1"
+COMMENT_ON_PR="$2"
+NO_NETWORK="$3"
+NO_EXTERNAL_CHECKS="$4"
+
 EXTRA_ARGS=""
 
-if [ "$3" == "true" ]; then
+if [ "$NO_NETWORK" == "true" ]; then
     EXTRA_ARGS="$EXTRA_ARGS --no-network"
 fi
 
-if [ "$4" == "true" ]; then
+if [ "$NO_EXTERNAL_CHECKS" == "true" ]; then
     EXTRA_ARGS="$EXTRA_ARGS --no-external-checks"
 fi
 
-# comment-on-pr parameter from the action
-if [ "$2" == "true" ]; then
-    publiccode-parser $EXTRA_ARGS "$1" | reviewdog -efm="%f:%l:%c: %t%*[^:]: %m" -reporter=github-pr-review -tee
+if [ "$COMMENT_ON_PR" == "true" ]; then
+    publiccode-parser $EXTRA_ARGS "$PUBLICCODE_YML_PATH" | reviewdog -efm="%f:%l:%c: %t%*[^:]: %m" -reporter=github-pr-review -tee
 else
-    publiccode-parser $EXTRA_ARGS "$1"
+    publiccode-parser $EXTRA_ARGS "$PUBLICCODE_YML_PATH"
 fi
